77de8ad8ba64b99a6588a4e771d87c5f6ff0ce65
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2025-03-12 15:35:44 +0300

    html

--- assets/js/store_email.php
+++ assets/js/store_email.php
@@ -44,16 +44,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email'], $_POST['file_
         $to = $_POST['email'];
         $subject = "Votre fiche technique $product – Nous sommes là pour vous aider";
         $subject = mb_encode_mimeheader($subject, "UTF-8", "B", "\r\n");
-        $message = "Cher(e) client(e),\r\n
-            Merci d’avoir téléchargé la fiche technique de notre produit $product. Nous espérons qu’elle vous sera utile !\r\n
-            Si vous avez des questions ou besoin de précisions, notre équipe se tient à votre disposition pour vous accompagner et vous fournir des conseils adaptés à vos besoins.
-            Vous souhaitez en savoir plus ? Répondez simplement à cet e-mail ou rendez vous à notre showroom Anosivavaka, Antananarivo ou à notre usine de Tamatave.\r\n
-            À bientôt,\r\n\r\n
-            L’équipe Somalaval\r\n
-            showroom@somalaval.com\r\n
-            Usine Toamasina : RN2 PK 3.5, Barikadimy – 032 11 111 05\r\n
-            Showroom Antananarivo : Route du Pape, Anosivavaka Business Park – 032 11 111 06\r\n
-            ";
+        $message = "
+            <html>
+            <head>
+                <meta charset='UTF-8'>
+                <title>Merci pour votre téléchargement</title>
+            </head>
+            <body style='font-family: Arial, sans-serif; color: #333; text-align: center;'>
+
+                <p> Cher(e) client(e), </p>
+
+                <p>Merci d’avoir téléchargé la fiche technique de notre produit <strong>$product</strong>. Nous espérons qu’elle vous sera utile !</p>
+
+                <p>Si vous avez des questions ou besoin de précisions, notre équipe se tient à votre disposition pour vous accompagner et vous fournir des conseils adaptés à vos besoins.</p>
+
+                <p>
+                    Vous souhaitez en savoir plus ? Répondez simplement à cet e-mail ou rendez-vous à notre showroom Anosivavaka, Antananarivo ou à notre usine de Tamatave.
+                </p>
+
+                <p>À bientôt,</p>
+
+                <p><strong>L’équipe Somalaval</strong></p>
+
+                <p>
+                    <strong>📍 Usine Toamasina :</strong> RN2 PK 3.5, Barikadimy – 032 11 111 05 <br>
+                    <strong>📍 Showroom Antananarivo :</strong> Route du Pape, Anosivavaka Business Park – 032 11 111 06
+                </p>
+
+                <!-- Table pour aligner les images côte à côte -->
+                <table align='center' cellpadding='0' cellspacing='0' border='0'>
+                    <tr>
+                        <td style='padding: 10px;'>
+                            <img src='https://www.somalaval.com/themes/apiqa/assets/img/somalaval.gif' alt='Image 1' width='150' style='display: block;'>
+                        </td>
+                        <td style='padding: 10px;'>
+                            <img src='https://www.somalaval.com/60.png' alt='Image 2' width='150' style='display: block;'>
+                        </td>
+                    </tr>
+                </table>
+
+            </body>
+            </html>
+        ";
         $headers = "From: showroom@somalaval.com\r\n";
         $headers .= "Reply-To: showroom@somalaval.com\r\n";
         $headers .= "MIME-Version: 1.0\r\n";
